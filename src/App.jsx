121ffb047ffc80bdcfb8a5ffbea6a828f86104cb
Peter <ppak10@gmail.com>
2020-02-06 19:58:21 -0800

    Adds basic element responses from imported data

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,40 @@
 // Node Modules
 import React from 'react';
 
+// Components
+import Item from './Item';
+
+// Data
+import DATA from './data';
+const {brands} = DATA[0];
+const {items} = brands[0].storeGroups[0].stores[0].menus[0].campaignMenuAudits[0];
+
 export default function App() {
+  console.log(brands, brands[0].name, items)
   return (
     <div className="app">
       <nav className="navigation-bar">
         <div className="restaurant-info">
-          <span>Restaurant Name</span>
-          <span>Restaurant Address</span>
+          <span>{brands[0].name || 'Restaurant Name'}</span>
+          <span>{brands[0].storeGroups[0].stores[0].address || 'Restaurant Name'}</span>
         </div>
         <span className="user-profile" />
       </nav>
-      <main>
-        Main
+      <main className="main-content">
+        <div className="item-header">
+          <span>Category Name</span>
+          <span>Showing 13 Items</span>
+        </div>
+        <div className="item-list">
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+          <Item />
+        </div>
       </main>
     </div>
   )
---
+++ src/Item.jsx
@@ -0,0 +1,13 @@
+/**
+ * Item.jsx
+ * Component to display item information
+ */
+
+// Node Modules
+import React from 'react';
+
+export default function Item() {
+  return (
+    <div>Hello</div>
+  )
+}
